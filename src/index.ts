441b4b3cbc8df65f4886bccf6f5b5d061bcc6c86
jjpegado <julio.tenorio_11@hotmail.com>
2026-02-27 14:35:47 -0300

    configuração e conexção com o mongo e mongoose

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+const dbUri = process.env.DB_URI as string;
+
+mongoose.connect(dbUri).then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    const app = express();
+
+    app.use(express.json());
+    app.get('/', (req, res) => {
+        return res.json( 'Tudo certo!')
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.PORT}`);
+    });
+
+}).catch((error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
+});
+
